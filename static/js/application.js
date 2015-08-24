e71adadf7b52e3e33f29ba42f70782a5e3f0fe14
Henrique Gogó <henriquegogo@gmail.com>
2015-08-24 02:09:08 -0300

    Organizing initializer

--- static/js/application.js
+++ static/js/application.js
@@ -3,35 +3,34 @@ var app = angular.module('ascii-warehouse', []);
 app.controller('ProductsController', function($scope) {
     var controller = this;
 
-    controller.all = [
-        {"id":"0-dslj5zvsd1jor","size":36,"price":589,"face":"( .-. )","date":"Sun Aug 16 2015 00:43:55 GMT-0300 (BRT)"},
-        {"id":"1-do2hswkffde7b9","size":19,"price":44,"face":"( .o.)","date":"Tue Aug 11 2015 12:53:06 GMT-0300 (BRT)"},
-        {"id":"2-eds4hwkc2u3m1jor","size":35,"price":198,"face":"( `·´ )","date":"Mon Aug 17 2015 02:54:46 GMT-0300 (BRT)"},
-        {"id":"3-xdbavl9p6g06bt9","size":19,"price":26,"face":"( ° ͜ ʖ °)","date":"Sun Aug 16 2015 02:53:03 GMT-0300 (BRT)"},
-        {"id":"4-e8pvl75su19bpgb9","size":33,"price":300,"face":"( ͡° ͜ʖ ͡°)","date":"Wed Aug 12 2015 13:06:01 GMT-0300 (BRT)"},
-        {"id":"5-x2a7fnlwxy0q9f6r","size":36,"price":379,"face":"( ⚆ _ ⚆ )","date":"Sun Aug 16 2015 02:34:16 GMT-0300 (BRT)"},
-        {"id":"6-wmqdxwzd57e3ik9","size":25,"price":656,"face":"( ︶︿︶)","date":"Thu Aug 13 2015 12:19:08 GMT-0300 (BRT)"},
-        {"id":"7-m49inewtphqia4i","size":33,"price":597,"face":"( ﾟヮﾟ)","date":"Wed Aug 12 2015 01:35:36 GMT-0300 (BRT)"},
-        {"id":"8-wktdm24oloumzpvi","size":27,"price":682,"face":"(\\/)(°,,,°)(\\/)","date":"Sun Aug 16 2015 09:12:59 GMT-0300 (BRT)"},
-        {"id":"9-jha7p87k20veipb9","size":35,"price":96,"face":"(¬_¬)","date":"Sun Aug 09 2015 02:20:54 GMT-0300 (BRT)"}
-    ];
+    controller.all = [];
 
-    function ndjsonToJson(ndjson) {
-        var jsonArrayStrings = ndjson.split('\n');
-        var jsonArrayObjects = [];
-        
-        for (var i in jsonArrayStrings) {
-            if (jsonArrayStrings[i]) jsonArrayObjects[i] = JSON.parse(jsonArrayStrings[i]);
-        }
-
-        return jsonArrayObjects;
+    controller.init = function() {
+        controller.fetchProducts();
     }
 
-    $.ajax('/api/products')
-        .complete(function(data) {
-            controller.all = ndjsonToJson(data.responseText);
-            $scope.$apply();
-        });
+    controller.setProducts = function(products) {
+        controller.all = products;
+        $scope.$apply();
+    }
 
-   // $http.get('/api/products');
+    controller.fetchProducts = function() {
+        function ndjsonToJson(ndjson) {
+            var jsonArrayStrings = ndjson.split('\n');
+            var jsonArrayObjects = [];
+            
+            for (var i in jsonArrayStrings) {
+                if (jsonArrayStrings[i]) jsonArrayObjects[i] = JSON.parse(jsonArrayStrings[i]);
+            }
+
+            return jsonArrayObjects;
+        }
+
+        $.ajax('/api/products')
+            .complete(function(data) {
+                controller.setProducts(ndjsonToJson(data.responseText))
+            });
+    }
+
+    controller.init();
 });
