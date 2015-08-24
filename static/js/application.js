217a01481c3905ac4a0d2319e96f0bc462714a7b
Henrique Gogó <henriquegogo@gmail.com>
2015-08-24 02:49:07 -0300

    Using  instead of 'this' variables

--- static/js/application.js
+++ static/js/application.js
@@ -1,42 +1,54 @@
 var app = angular.module('ascii-warehouse', []);
 
 app.controller('ProductsController', function($scope) {
-    var controller = this;
-    controller.all = [];
+    var apiUrl = '/api/products';
+    var total = 15;
+
+    $scope.products = [];
 
     function init() {
         fetchProducts();
     }
 
     function setProducts(products) {
-        controller.all = products;
+        $scope.products = products;
         $scope.$apply();
     }
 
-    function fetchProducts(sortBy) {
-        function ndjsonToJson(ndjson) {
-            var jsonArrayStrings = ndjson.split('\n');
-            var jsonArrayObjects = [];
-            
-            for (var i in jsonArrayStrings) {
-                if (jsonArrayStrings[i]) jsonArrayObjects[i] = JSON.parse(jsonArrayStrings[i]);
-            }
-
-            return jsonArrayObjects;
+    function addProducts(moreProducts) {
+        for (var i in moreProducts) {
+            $scope.products.push(moreProducts[i]);
         }
-
-        var url = '/api/products';
-        if (sortBy) url = url + '?sort=' + sortBy;
-
-        $.ajax(url)
-            .complete(function(data) {
-                setProducts(ndjsonToJson(data.responseText))
-            });
+        $scope.$apply();
     }
 
-    controller.fetchBySize = function() { fetchProducts('size'); }
-    controller.fetchByPrice = function() { fetchProducts('price'); }
-    controller.fetchById = function() { fetchProducts('id'); }
+    function ndjsonToJson(ndjson) {
+        var jsonArrayStrings = ndjson.split('\n');
+        var jsonArrayObjects = [];
+        
+        for (var i in jsonArrayStrings) {
+            if (jsonArrayStrings[i]) jsonArrayObjects[i] = JSON.parse(jsonArrayStrings[i]);
+        }
+
+        return jsonArrayObjects;
+    }
+
+    function fetchProducts(sortBy) {
+        var url = apiUrl;
+        if (sortBy) url = url + '?sort=' + sortBy;
+
+        $.ajax(url).complete(function(data) {
+            setProducts(ndjsonToJson(data.responseText))
+        });
+    }
+
+    $scope.sortBy = function(filter) {
+        var url = apiUrl + '?sort=' + filter + '';
+
+        $.ajax(url).complete(function(data) {
+            setProducts(ndjsonToJson(data.responseText))
+        });
+    }
 
     init();
 });
